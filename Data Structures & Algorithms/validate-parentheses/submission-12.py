55fea431fd009136f5d27d8764c3a08e494b8614
Chuying Huo <chuying.huo@duke.edu>
2026-09-07 22:51:22 -0400

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        parentheses = { '}': '{', ')': '(', ']': '['}
+        stack = []
+
+        for letter in s:
+            if letter in '({[':
+                stack.append(letter)
+        
+            elif letter in parentheses:
+                if not stack or stack.pop() != parentheses[letter]:
+                    return False 
+
+        return not stack 
+
+        
